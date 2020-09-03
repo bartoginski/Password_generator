614f348340f262ca230303e127e47e825fe4759b
bartoginski <ogitoziomal@gmail.com>
2020-09-03 19:05:23 +0200

    program saves data into .txt file correctly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from os import path
 
 
 def get_properties():
@@ -8,22 +9,39 @@ def get_properties():
     for i in range(int(characters_num)):
         new_string += random.choice(all_strings)
     print(new_string)
+    return new_string
 
 
-# Password generator
-# with open('some_file.txt', 'w') as file:
-#     file.write('Password Generator!\n')
-#     file.write('====================\n')
-#     file.write('\n')
+def is_exist(filename):
+    return path.exists(filename)
 
 
+def write_file(password, username, webpage):
+    """
+    Writes password, username, webpage into a .txt file
+    :param password: generated password
+    :param username: username string
+    :param webpage: webpage string
+    :return: None
+    """
 
-get_properties()
+    with open('passwords.txt', 'a') as file:
+        file.write('====================\n')
+        file.write(f"Webpage: {webpage}\n")
+        file.write(f"Username: {username}\n")
+        file.write(f"Password: {password}\n")
+        file.write('====================\n')
 
 
-# >>> import string
-# >>> string.ascii_letters
-# 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# >>> import random
-# >>> random.choice(string.ascii_letters)
-# 'j'
+def main():
+    print('Hello user!')
+    webpage = input('Enter a webpage name: ')
+    username = input('Enter your username: ')
+    password = get_properties()
+    write_file(password, username, webpage)
+    print("Your passwords are into passwords.txt file")
+
+
+main()
+
+
